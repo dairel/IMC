b43b0ae0d6652ce0107c16f53442550a1611a08f
dairel <dairelsi@live.com>
2015-05-12 21:19:46 -0300

    004

---
+++ java/imc.js
@@ -0,0 +1,18 @@
+var IMCService = {
+  
+    calculate: function(height, weight)
+    {
+        
+        
+    },
+    
+    
+    //todo to implemented
+    
+      description: function(index){
+      
+      }
+    
+    
+    
+};
